db2b2bc79ce4eb0c4fcfb25466208b7182285b20
fz1989 <fz1989fz@gmail.com>
2013-02-01 17:03:03 +0800

    最后版

---
+++ XDUGraduateLogin.py
@@ -0,0 +1,44 @@
+﻿import re
+import urllib2
+import urllib
+import cookielib
+def autoLogin(username, pwd):
+	'''处理自动登录的脚本，返回的是学号和个人的课程编号'''
+	cookie=cookielib.CookieJar()
+	cj=urllib2.HTTPCookieProcessor(cookie)
+	opener = urllib2.build_opener(cj)
+	postdata = {'j_username':username,
+				'j_password':pwd,
+				}
+	#登录的第一步
+	req = urllib2.Request('http://210.27.12.1:90/student/index.jsp')
+	operate = opener.open(req)
+	#登录的第二步，填写用户名和密码
+	req = urllib2.Request('http://210.27.12.1:90/j_security_check', urllib.urlencode(postdata))
+	operate = opener.open(req)
+	#登录成功，获取课程编号
+	req = urllib2.Request('http://210.27.12.1:90/viewStudyPlanAction.do')
+	list = []
+	p = re.compile(r'\d{7}')
+	operate = opener.open(req)
+	for m in p.finditer(operate.read()):
+		list.append(m.group())
+	list = list[1:]
+	
+	#登录成功，获取学号
+	req = urllib2.Request('http://210.27.12.1:90/student/main.jsp')
+	operate = opener.open(req)
+	p = re.compile(r'xdleess\d{8}sn\d{4}')
+	content = operate.read()
+	match = p.search(content)
+	
+	#登录成功，获取整体信息
+	p = re.compile(r'\d+\.\d+')
+	info = {};
+	val = p.findall(content)
+	info[u'全部额定学分'] = val[0];
+	info[u'全部额定学分已完成'] = val[1];
+	info[u'学位课额定学分'] = val[2];
+	info[u'学位课额定学分已完成'] = val[3];
+	info[u'学位课加权平均分'] = val[4];
+	return list, match.group(),info
---
+++ XDUGraduateQueryScore.py
@@ -0,0 +1,60 @@
+﻿import re
+import urllib2
+import urllib
+import cookielib
+import re
+from XDUGraduateLogin import autoLogin
+# http://210.27.12.1:90/queryDegreeScoreAction.do?studentid=xdleess20120514sn1585&degreecourseno=0821005
+
+def getScore(stu, cno):
+	'''获得单科的数据'''
+	url = 'http://210.27.12.1:90/queryDegreeScoreAction.do?'
+	#处理cookie
+	cookie=cookielib.CookieJar()
+	cj=urllib2.HTTPCookieProcessor(cookie)
+	opener = urllib2.build_opener(cj)
+	#填写表单
+	postdata = 'studentid=' + stu + '&degreecourseno='+cno
+	req = urllib2.Request(url + postdata)
+	operate = opener.open(req)
+	#获得数据分析html，太麻烦了this copy from dccrazyboy
+	content = operate.read()
+	start = content.find('<tbody>')
+	end = content.find('</tbody>')
+	tbody = content[start:end]
+	val = []
+	while '</td>' in tbody:
+		end = tbody.find('</td>')
+		start = tbody.rfind('>',0,end)
+		val.append(tbody[start+1:end].strip())
+		tbody = tbody[end+len('</td>'):]
+	return val
+
+def getAllScore(stu,courses):
+	'''获得全部的数据'''
+	scores = []
+	for course in courses:
+		scores.append(getScore(stu,course))
+	return scores
+
+if __name__ == '__main__':
+	print u'学号：'
+	stuno = raw_input()
+	print u'密码：'
+	stupw = raw_input()
+	print u'拼命登录中。。。'
+	cno,sno,info = autoLogin(stuno, stupw)
+	print u'登录完毕。。。'
+
+	print u'拼命加载中。。。'
+	scores = getAllScore(sno,cno)
+	print u'加载完毕。。。'
+	print "============================================================"
+	for (k,v) in info.iteritems():
+		print "%s:%s" % (k,v)
+	print "============================================================"
+	print u'课程名称,学分,成绩'
+	for each in scores:
+		if each[4]:
+			print "%s,%s,%s" % (each[1],each[2],each[4])
+	print "============================================================"
